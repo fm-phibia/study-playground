57d4cf8258c3bd160069cbab5b96ddda19c18754
fumiyamatuura <f.tbh.depapls238@gmail.com>
2024-04-20 22:31:07 +0900

    ✨ C++の実行環境作成

---
+++ cpp/algo-shiki/hello.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+    cout << "Input: ";
+    string data;
+    getline(cin, data);
+    cout << "Output: " << data << "\n\n";
+    return 0;
+}
