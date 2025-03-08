1a98f9435792974a2f37b60b737f113aa5b3d18f
F-Matsuura <f.matsuura@gakken.co.jp>
2025-03-08 09:53:16 +0900

    serverless framework追加

---
+++ serverless-framework/.devcontainer/create.sh
@@ -0,0 +1,8 @@
+# !bin/bash
+echo 'alias ll="ls -l"' >> ~/.bashrc
+echo 'alias la="ls -al"' >> ~/.bashrc
+echo 'alias nrd="npm run dev"' >> ~/.bashrc
+echo 'alias nrg="npm run generate"' >> ~/.bashrc
+echo 'alias nrs="npm run story:dev"' >> ~/.bashrc
+
+yarn global add serverless
